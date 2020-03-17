e64344efcd7542997acbe54eede532b3ceedaee2
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-17 14:08:40 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,7 @@ class StudentsController < ApplicationController
 
   def create
     @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    binding.pry
     redirect_to "/students/#{@student.id}"
   end
 end
-
